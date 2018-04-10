08af6125a579569e99ad83aa6030e96bca297b6f
Manish Patiyal <patiyal.manish@hotmail.com>
2018-04-11 01:07:43 +0530

    Changes to make app running

--- app/src/main/java/com/pubnub/tutorials/map/android/pubnubmaptutorialandroid/Constants.java
+++ app/src/main/java/com/pubnub/tutorials/map/android/pubnubmaptutorialandroid/Constants.java
@@ -1,8 +1,8 @@
 package com.pubnub.tutorials.map.android.pubnubmaptutorialandroid;
 
 public class Constants {
-    public static final String PUBNUB_PUBLISH_KEY = "YOUR_PUB_KEY";
-    public static final String PUBNUB_SUBSCRIBE_KEY = "YOUR_SUB_KEY";
+    public static final String PUBNUB_PUBLISH_KEY = "pub-c-cf404789-f5af-4cf6-a9ae-74144a04c792";
+    public static final String PUBNUB_SUBSCRIBE_KEY = "sub-c-fb84b9ea-3ceb-11e8-8bb7-3ab51ec5ed79";
 
     public static final String SUBSCRIBE_CHANNEL_NAME = "basic_location_subscribe";
     public static final String PUBLISH_CHANNEL_NAME = "basic_location_publish";
